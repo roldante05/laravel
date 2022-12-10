9c71c5ca15974ca714dc2491ea229b6425cc05b9
Dante Roldan <danteroldan72@gmail.com>
2022-12-10 13:37:01 -0300

    controller home

--- burger2022/app/Http/Controllers/ControladorHome.php
+++ burger2022/app/Http/Controllers/ControladorHome.php
@@ -12,17 +12,18 @@ class ControladorHome extends Controller
     public function index(){
       
         $titulo="Inicio";
-        if (Usuario::autenticado() == true) {
-            if (!Patente::autorizarOperacion("MENUCONSULTA")) {
-                $codigo = "MENUCONSULTA";
-                $mensaje = "No tiene permisos para la operaci&oacute;n.";
-                return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
-            } else {
-                return view('sistema.index', compact('titulo'));
-            }
-        } else {
-            return redirect('admin/login');
-        }
+        return view('sistema.index', compact('titulo'));
+        // if (Usuario::autenticado() == true) {
+        //     if (!Patente::autorizarOperacion("MENUCONSULTA")) {
+        //         $codigo = "MENUCONSULTA";
+        //         $mensaje = "No tiene permisos para la operaci&oacute;n.";
+        //         return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
+        //     } else {
+        //         return view('sistema.index', compact('titulo'));
+        //     }
+        // } else {
+        //     return redirect('admin/login');
+        // }
     }
 
 }
